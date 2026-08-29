d6cd494ee696f6b58e21350127ffccc0cff56d21
semajz <semajzh@gmail.com>
2026-08-29 11:51:06 +0800

    bug fix: 3080122

--- buff/buff3080122.cpp
+++ buff/buff3080122.cpp
@@ -5,7 +5,8 @@
 void Buff3080122::enter(Ground* ground)
 {
     Logger::H().printbuffenter(ground, src, des, this);
-    k0 = Ground::pointbystar(ground, 24, src, method);
+    float point1 = Ground::pointbystar(ground, 24, src, method);
+    k0 = Ground::addbyix(ground, point1, ground->m_group[src/10].m_item[src%10].i[1]);
     ground->m_group[des/10].m_item[des%10].k[0] -= k0;
 }
 
