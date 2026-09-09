6b685d032b85b632b17ac324478c2f2f5587217e
semajz <semajzh@gmail.com>
2026-09-09 13:32:59 +0800

    update: 309011

--- buff/buff309011.cpp
+++ buff/buff309011.cpp
@@ -5,7 +5,7 @@
 void Buff309011::enter(Ground* ground)
 {
     Logger::H().printbuffenter(ground, src, des, this);
-    j7 = Ground::addbyix(ground, 8, ground->m_group[src/10].m_item[src%10].i[0]);
+    j7 = Ground::addbyix(ground, 8, ground->m_group[src/10].m_item[src%10].i[0]+ground->m_group[src/10].m_item[src%10].i[3]);
     ground->m_group[des/10].m_item[des%10].j[7] += j7;
 }
 
