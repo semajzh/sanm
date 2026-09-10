31405780115779e3e02b6a0af5b3fec63009c6be
semajz <semajzh@gmail.com>
2026-09-10 20:44:42 +0800

    bug fix: 1370

--- buff/buff001370.cpp
+++ buff/buff001370.cpp
@@ -5,7 +5,7 @@
 void Buff001370::enter(Ground* ground)
 {
     Logger::H().printbuffenter(ground, src, des, this);
-    l1 = Ground::addln(ground, des, 0, -9);
+    l1 = Ground::addln(ground, des, 1, -9);
     ground->m_group[des/10].m_item[des%10].l[1] += l1;
 }
 
